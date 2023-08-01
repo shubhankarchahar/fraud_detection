5f887cdb5f572e31bd94a9d4e78d84d1f0a4c0ce
Shubhankar2508 <s.chahar2712@gmail.com>
2023-08-01 18:54:47 +0530

    addded 3 ml models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("adding all ml related code here")
+
+
+def model():
+    print('adidng code for mode 1')
+
+def model():
+    print('adidng code for mode 1')
+
+def model():
+    print('adidng code for mode 1')
