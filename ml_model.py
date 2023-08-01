aa57bbb5b7645e5384680dabc3ab3727e2c40c72
Shubhankar2508 <s.chahar2712@gmail.com>
2023-08-01 18:57:10 +0530

    model2 is the final model

--- ml_model.py
+++ ml_model.py
@@ -4,8 +4,10 @@ print("adding all ml related code here")
 def model():
     print('adidng code for mode 1')
 
-def model():
-    print('adidng code for mode 1')
+def model2():
+    print('adidng code for mode 2')
 
-def model():
-    print('adidng code for mode 1')
+def model3():
+    print('adidng code for mode 3')
+
+print('model2 is the final model')
